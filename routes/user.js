30eea482fe1ab3a58d15fbb8f723aa398fe5b772
Parwat Kunwar <parwatkunwar08@gmail.com>
2016-11-26 21:54:11 +0545

    photos data links thru albums dn

--- routes/user.js
+++ routes/user.js
@@ -4,9 +4,9 @@ var request = require('request');
 
 var User = require('../models/users');
 
-router.get('/users', function(req, res, next) {
-    User.find({}, function(err, res) {
-        if(err) {
+router.get('/users', function (req, res, next) {
+    User.find({}, function (err, res) {
+        if (err) {
             throw new Error(err);
         }
         else {
@@ -15,32 +15,35 @@ router.get('/users', function(req, res, next) {
     });
 });
 
-router.get('/images', function(req, res, next) {
+router.get('/images', function (req, res, next) {
     var accesstoken = req.user.token;
-    var url = "https://graph.facebook.com/me/photos";
+    var c = "count,name";
+    var url = "https://graph.facebook.com/me/albums";
     request.get({
         url: url,
-        qs: {access_token: accesstoken},
-         json: true
-    }, function(err, resp, data) {
-        if(err) {
+        qs: {
+            access_token: accesstoken,
+            fields: c
+        },
+        json: true
+    }, function (err, resp, data) {
+        if (err) {
             throw new Error(err);
         } else {
-            console.log("adadada", data);
-            res.json(data);
+            res.render('albums', { albums: data });
         }
     });
 });
 
-router.get("/posts", function(req, res, next) {
-    var accesstoken= req.user.token;
+router.get("/posts", function (req, res, next) {
+    var accesstoken = req.user.token;
     var url = "https://graph.facebook.com/me/posts";
     request.get({
         url: url,
-        qs: {access_token: accesstoken},
+        qs: { access_token: accesstoken },
         json: true
-    }, function(err, resp, data) {
-        if(err) {
+    }, function (err, resp, data) {
+        if (err) {
             throw new Error(err);
         } else {
             res.json(data);
@@ -48,12 +51,32 @@ router.get("/posts", function(req, res, next) {
     });
 });
 
-router.get("/pagesliked", function(req, res, next) {
+router.get("/pagesliked", function (req, res, next) {
     var at = req.user.token;
     var c = "context";
     var url = "https://graph.facebook.com/me";
     request.get({
-        url:url,
+        url: url,
+        qs: {
+            access_token: at,
+            fields: c
+        },
+        json: true
+    }, function (err, resp, data) {
+        if (err) {
+            throw new Error(err);
+        } else {
+            res.json(data);
+        }
+    });
+});
+
+router.get("/albumphotos/:id", function(req, res, next) {
+    var at = req.user.token;
+    var c = "photos{link}";
+    var url = "https://graph.facebook.com/"+ req.params.id;
+    request.get({
+        url: url,
         qs: {
             access_token: at,
             fields: c

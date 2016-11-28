09cfbc30635b59826ec1aa81d8fc0f4afe89e7a1
Parwat Kunwar <parwatkunwar08@gmail.com>
2016-11-28 21:17:43 +0545

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,8 +2,8 @@ module.exports = {
     'url': "mongodb://localhost:27017/fb",
     'key': '1213414141-414914',
     'facebook': {
-        'clientID': '375509292794901',
-        'clientSecret': 'b1175fc31a6ee3e47c196f414fecbfd1',
+        'clientID': 'Your Client ID',
+        'clientSecret': 'Your Client Secret',
         'callbackURL': 'http://localhost:3000/auth/facebook/callback'
     }
 };
